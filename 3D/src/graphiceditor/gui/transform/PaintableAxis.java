90ceb3200e24517d0a5d7af5b4c55021665c6e05
Niels Gundermann <gundermann.niels.ng@googlemail.com>
2014-05-15 09:00:26 +0200

    improved axis

--- 3D/src/graphiceditor/gui/transform/PaintableAxis.java
+++ 3D/src/graphiceditor/gui/transform/PaintableAxis.java
@@ -6,19 +6,25 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.LineBuilder;
 import javafx.scene.transform.Rotate;
+import javafx.scene.transform.RotateBuilder;
 
-public class PaintableAxis extends Axis{
+public class PaintableAxis extends Axis {
 
 	private Line axisShape;
 
-	public PaintableAxis(Point3D pivot, Point3D direction, AngleProperty angleProperty) {
+	public PaintableAxis(Point3D pivot, Point3D direction,
+			AngleProperty angleProperty) {
 		super(pivot, direction, angleProperty);
 		axisShape = LineBuilder.create().startX(pivot.getX())
 				.startY(pivot.getY()).endX(direction.getX() + pivot.getX())
 				.endY(direction.getY() + pivot.getY()).build();
-		if (direction.getZ() != 0)
-			axisShape.getTransforms().add(new Rotate(-90, pivot));
-		axisShape.translateZProperty().add(pivot.getZ());
+		if (direction.getZ() != 0){
+			axisShape.setEndX(1000);
+			axisShape.getTransforms().add(
+					RotateBuilder.create().axis(Rotate.Y_AXIS)
+							.pivotX(getPivotX()).pivotY(getPivotY())
+							.pivotZ(getPivotZ()).angle(90).build());
+		}
 		axisShape.setFill(Color.RED);
 	}
 

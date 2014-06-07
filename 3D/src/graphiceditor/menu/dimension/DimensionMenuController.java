61216837454ad2aeaebb683d1cc0bc5c7708b54a
Niels Gundermann <gundermann.niels.ng@googlemail.com>
2014-06-07 15:14:47 +0200

    Rotation slider fixed

--- 3D/src/graphiceditor/menu/dimension/DimensionMenuController.java
+++ 3D/src/graphiceditor/menu/dimension/DimensionMenuController.java
@@ -1,15 +1,29 @@
 package graphiceditor.menu.dimension;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import graphiceditor.gui.DimensionArea;
 import graphiceditor.menu.MenuDisabler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.control.ToggleButton;
 import javafx.util.converter.NumberStringConverter;
 
-public class DimensionMenuController{
+public class DimensionMenuController implements Initializable {
 
 	private DimensionArea dimensionArea;
+
+	@FXML
+	private Slider slXRotation;
+
+	@FXML
+	private Slider slYRotation;
+
+	@FXML
+	private Slider slZRotation;
 
 	@FXML
 	private ToggleButton btXRotation;
@@ -30,21 +44,49 @@ public class DimensionMenuController{
 	private Label lbZRotation;
 
 	public void setDimensionArea(DimensionArea dimensionArea) {
+		if (this.dimensionArea != null)
+			resetSliders();
 		this.dimensionArea = dimensionArea;
-		lbXRotation.textProperty().bindBidirectional(dimensionArea.getXRotationProperty(), new NumberStringConverter());
-		lbYRotation.textProperty().bindBidirectional(dimensionArea.getYRotationProperty(), new NumberStringConverter());
-		lbZRotation.textProperty().bindBidirectional(dimensionArea.getZRotationProperty(), new NumberStringConverter());;
+		updateSliders();
+	}
+
+	private void updateSliders() {
+		slXRotation.valueProperty().setValue(
+				dimensionArea.getXRotationProperty().getValue());
+		slYRotation.valueProperty().setValue(
+				dimensionArea.getYRotationProperty().getValue());
+		slZRotation.valueProperty().setValue(
+				dimensionArea.getZRotationProperty().getValue());
+		slXRotation.valueProperty().bindBidirectional(
+				dimensionArea.getXRotationProperty());
+		slYRotation.valueProperty().bindBidirectional(
+				dimensionArea.getYRotationProperty());
+		slZRotation.valueProperty().bindBidirectional(
+				dimensionArea.getZRotationProperty());
+	}
+
+	private void resetSliders() {
+		slXRotation.valueProperty().unbindBidirectional(
+				dimensionArea.getXRotationProperty());
+		slYRotation.valueProperty().unbindBidirectional(
+				dimensionArea.getYRotationProperty());
+		slZRotation.valueProperty().unbindBidirectional(
+				dimensionArea.getZRotationProperty());
+		slXRotation.valueProperty().set(0);
+		slYRotation.valueProperty().set(0);
+		slZRotation.valueProperty().set(0);
 	}
 
 	@FXML
-	public void resetDimensions(){
+	public void resetDimensions() {
 		dimensionArea.resetDimensions();
 	}
-	 @FXML
-	  public void showAxisOfElement(){
+
+	@FXML
+	public void showAxisOfElement() {
 		dimensionArea.showAxis();
-	  }
-	
+	}
+
 	@FXML
 	public void toggleXRotation() {
 		if (!btXRotation.isSelected()) {
@@ -91,6 +133,16 @@ public class DimensionMenuController{
 			btYRotation.setDisable(true);
 			btXRotation.setDisable(true);
 		}
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		lbXRotation.textProperty().bindBidirectional(
+				slXRotation.valueProperty(), new NumberStringConverter());
+		lbYRotation.textProperty().bindBidirectional(
+				slYRotation.valueProperty(), new NumberStringConverter());
+		lbZRotation.textProperty().bindBidirectional(
+				slZRotation.valueProperty(), new NumberStringConverter());
 	}
 
 }

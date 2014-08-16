3363fb89bcb9ae2e2969bcfc2736dc604e593e92
Niels Gundermann <gundermann.niels.ng@googlemail.com>
2014-08-16 09:51:29 +0200

    animation refactored and uses keyframes.

--- 3D/src/graphiceditor/business/Animation.java
+++ 3D/src/graphiceditor/business/Animation.java
@@ -6,6 +6,4 @@ public interface Animation extends Runnable{
 
 	void addProperty(AnimationProperty property);
 
-	void start();
-	
 }
--- 3D/src/graphiceditor/business/impl/SingleAnimationImpl.java
+++ 3D/src/graphiceditor/business/impl/SingleAnimationImpl.java
@@ -1,11 +1,8 @@
 package graphiceditor.business.impl;
 
-import graphiceditor.AnimationFactory;
 import graphiceditor.business.Animation;
-import graphiceditor.business.CommonObject3D;
 import graphiceditor.domainspecific.values.observable.AnimationProperty;
 import graphiceditor.domainspecific.values.observable.ChangeGraphicProperty;
-import graphiceditor.util.ShapeManipulater;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,45 +41,9 @@ public class SingleAnimationImpl implements Animation {
 		return keyFrameList ;
 	}
 
-	private List<ChangeGraphicProperty> extractPartialChanges(
-			AnimationProperty property) {
-		double time = property.getTime();
-		List<ChangeGraphicProperty> changedProperties = property
-				.getChangedProperties();
-		List<ChangeGraphicProperty> partialChanges = new ArrayList<ChangeGraphicProperty>();
-		for (ChangeGraphicProperty changeGraphicProperty : changedProperties) {
-			Double value = changeGraphicProperty.getValue();
-			Double partialChangeValue = value / (time / 100);
-			partialChanges.add(AnimationFactory.getInstance()
-					.createChangeGraphicProperty(
-							changeGraphicProperty.getPropertyName(),
-							partialChangeValue));
-		}
-		return partialChanges;
-	}
-
 	@Override
 	public void addProperty(AnimationProperty property) {
 		properties.add(property);
-	}
-
-	@Override
-	public void start() {
-		AnimationProperty property = properties.get(0);
-		double starttime = System.currentTimeMillis();
-		double endtime = starttime + property.getTime();
-		List<ChangeGraphicProperty> partialChanges = extractPartialChanges(property);
-		CommonObject3D animatedObject = property.getAnimatedObject();
-		while (endtime > starttime) {
-			ShapeManipulater.getInstance().invokeChangeAdditional(partialChanges,
-					animatedObject);
-//			try {
-//				Thread.sleep(100);
-				endtime = endtime - 100;
-//			} catch (InterruptedException e) {
-//				e.printStackTrace();
-//			}
-		}
 	}
 
 }
